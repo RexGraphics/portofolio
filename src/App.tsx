3e620fea85bb79861b7e402c0a62240ca63d2b6c
Ghazwan Fikri Maulana <ghazwanfikri524@gmail.com>
2025-10-28 02:45:56 +0700

    feat(ui): add WebGL backgrounds and animated text
    Introduces `Aurora` and `Particles` components for dynamic WebGL backgrounds, and `BlurText` for animated text effects. These are integrated into `App.tsx` to create a visually engaging landing page. Adds `motion` and `ogl` dependencies. Includes minor CSS updates and a directory rename.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Aurora from "./components/backgrounds/Aurora/Aurora";
+import Particles from "./components/backgrounds/Particles/Particles";
+import BlurText from "./components/cummons/BlurText/BlurText";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-black">
+      <div className="w-full h-full">
+        <Aurora colorStops={["#059669", "#102B20", "#D1FAE5"]} blend={1000} amplitude={1.0} speed={0.5} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="w-full h-full absolute top-0 left-0 flex justify-content-center align-items-center overflow-hidden">
+        <BlurText
+          text="Ghazwan"
+          delay={50}
+          animateBy="letters"
+          direction="top"
+          // onAnimationComplete={handleGlow}
+          className="text-9xl font-extrabold text-white m-auto"
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="w-full h-full absolute top-0 left-0">
+        <Particles />
+      </div>
+    </div>
   )
 }
 
